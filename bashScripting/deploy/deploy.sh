1713e7c64c416a0527a03715d78cbb45bc771183
Rafa <jchavarria@soin.co.cr>
2020-10-04 20:13:19 -0600

    added bithday chocolate hackerrank

---
+++ bashScripting/deploy/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# sh deploy.sh --migracion --dev --clean|--quick
+echo "You start with $# positional parameters"
+
+# Loop until all parameters are used up
+while [ "$1" != "" ]; do
+    if [ "$1" == "cheese" ]; then
+      
+    fi
+    shift
+
+done
--- hackerrank/birthday-chocolate.js
+++ hackerrank/birthday-chocolate.js
@@ -1,13 +1,24 @@
-  function birthday(s, d, m) {
-      const lenght = s.length;  
-    if ((lenght % 2) === 0) {
-        console.log('s', s, 'd', d, 'm', m);
-        console.log('its odd');
-      } else {
-          console.log('its not odd');
-          
-      }
+  function birthday(chocolateBar, day, month) {
+    const numbs = getConsecutiveValues(chocolateBar, month)
+    let possibleWays = 0
+    numbs.map(item => {
+      if(item === day) possibleWays++
+    })
+    console.log('possibleWays', possibleWays);
+    return possibleWays;
+  }
 
-}
+  function getConsecutiveValues(chocolateBar, consecutive) {
+    const nums = [];
+    for (let i = 0; i < chocolateBar.length; i++) {
+      let result = 0;
+        for (let j = 0; j < consecutive; j++) {
+          result = result + chocolateBar[i + j]
+        }
+        nums.push(result)
+    }
+    return nums
+  }
 
-birthday([ 1, 2, 1, 3, 2 ], 3, 2);
+birthday([ 5, 5, 3, 2, 2, 2, 1, 2, 5, 3, 5, 5, 4, 3, 3, 5 ], 13, 3);
+// birthday([ 5, 5, 3, 2, 2, 2, 1, 2, 5, 3, 5, 5, 4, 3, 3, 5], 13, 3);
